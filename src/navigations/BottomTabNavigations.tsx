1610bac294c15146964ff021e2f7c5206a934a57
Bakti Aulia Zikri <127172155+baktiauliazikri@users.noreply.github.com>
2024-06-30 19:50:38 +0700

    Update BottomTabNavigations.tsx

--- src/navigations/BottomTabNavigations.tsx
+++ src/navigations/BottomTabNavigations.tsx
@@ -1,25 +1,29 @@
 import React from 'react'
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs'
+
 import { Feather } from '@expo/vector-icons'
 import Home from '../screens/Home'
 import Search from '../screens/Search'
 import Favorite from '../screens/Favorite'
-const Tab = createBottomTabNavigator()
 import HomeStackNavigation from './HomeStackNavigation'
+import SearchStackNavigation from './SearchStackNavigation'
+
+const Tab = createBottomTabNavigator()
 
 const BottomTabNavigation = ():JSX.Element => {
-    return (
-      <Tab.Navigator>
-          <Tab.Screen name='homes' component={HomeStackNavigation} options={{ tabBarIcon:({color})=>(
-              <Feather name='home' size={28} color={color}/> 
-          ),headerShown:false }}/>
-          <Tab.Screen name='Search' component={Search} options={{ tabBarIcon:({color})=>(
-              <Feather name='search' size={28} color={color}/> 
-          ),headerShown:false }}/>
-          <Tab.Screen name='Favorite' component={Favorite} options={{ tabBarIcon:({color})=>(
-              <Feather name='heart' size={28} color={color}/> 
-          ),headerShown:false }}/>
-      </Tab.Navigator>
-    )
-  }
-  export default BottomTabNavigation
+  return (
+    <Tab.Navigator>
+        <Tab.Screen name='homes' component={HomeStackNavigation} options={{ tabBarIcon:({color})=>(
+            <Feather name='home' size={28} color={color}/> 
+        ),headerShown:false }}/>
+        <Tab.Screen name='Search' component={SearchStackNavigation} options={{ tabBarIcon:({color})=>(
+            <Feather name='search' size={28} color={color}/> 
+        ),headerShown:false }}/>
+        <Tab.Screen name='Favorite' component={Favorite} options={{ tabBarIcon:({color})=>(
+            <Feather name='heart' size={28} color={color}/> 
+        ),headerShown:false }}/>
+    </Tab.Navigator>
+  )
+}
+
+export default BottomTabNavigation
