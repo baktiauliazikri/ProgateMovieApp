8655d6126ba60d73d6e9e70bd78b86b4fd4c0ef4
Bakti Aulia Zikri <127172155+baktiauliazikri@users.noreply.github.com>
2024-06-30 19:50:14 +0700

    Update HomeStackNavigation.tsx

--- src/navigations/HomeStackNavigation.tsx
+++ src/navigations/HomeStackNavigation.tsx
@@ -2,15 +2,20 @@ import React from 'react';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import Home from '../screens/Home';
 import MovieDetail from '../screens/MovieDetail';
-const HomeStackNavigation = () => {
+import Genre from '../screens/Genre';
+import KeywordSearch from '../components/search/KeywordSearch';
+import Search from '../screens/Search';
+const SearchStackNavigation = () => {
 
 const Stack = createNativeStackNavigator() 
 return (
-      <Stack.Navigator initialRouteName='Home'>
-        <Stack.Screen name='Home' component={Home}  />
+      <Stack.Navigator initialRouteName='KeywordSearch'>
+        <Stack.Screen name='KeywordSearch' component={Search} />
         <Stack.Screen name='MovieDetail' component={MovieDetail} />
+        <Stack.Screen name='Home' component={Home}  />
+        <Stack.Screen name='Genre' component={Genre} />
       </Stack.Navigator>
   );
 }
 
-export default HomeStackNavigation
+export default SearchStackNavigation
